ad9f5ec3ddbcc968c50d7459e0641bcabf6e8953
why0810 <873500953@qq.com>
2014-10-28 23:06:03 +0800

    修改头文件

--- src/CommandPlat/Comm struct.h
+++ src/CommandPlat/Comm struct.h
@@ -54,8 +54,14 @@ typedef struct __ESMSTATUS_MARK
 	 double dConfidence;                 //可信度
      char cCountry[32];                  //国家（地区）
 
-	 char cJfFlag;
-	 unsigned long lFlag;                 //累加标记
+	unsigned long lJfFlag;               //聚类标记
+	unsigned long lFlag;                 //累加标记
+	//1016补充
+	unsigned long lFusFlag;                //融合查找标记
+
+	//10.20新增，ESM基本事件，模板号
+	int nESMEvt;                         //ESM基本事件
+	int nModel;                          //模板号
 
 }ESMSTATUS_MARK;
  
@@ -91,8 +97,14 @@ typedef struct __COMSTATUS_MARK           //通信侦查信息
 	char cDWAttribute[32];					  //敌我属性
 	double dConfidence;  			      //可信度
     char cCountry[32];			          //国家（地区）
-	char cJfFlag;
+	unsigned long lJfFlag;                //聚类标记
 	unsigned long lFlag;                  //累加标记
+//1016补充
+	unsigned long lFusFlag;                //融合查找标记
+
+	//10.20新增，COM基本事件，模板号
+	int nCOMEvt;                          //COM基本事件
+	int nModel;                           //COM模板号
 
 }COMSTATUS_MARK;
 
@@ -130,9 +142,11 @@ typedef struct __TRACKSTATUS_MARK        //雷达航迹和IFF信息
     char sPlatType[32];  
 	char cPlatName[32];                  //平台名称  
     double dConfidence;                  //可信度
-    char cDWAttribute;                   //敌我属性 (IFF信息)
-	char cJfFlag;                        //是否进行数据融合标志，0：未融合，1：已融合
+    char cDWAttribute[32];                   //敌我属性 (IFF信息)
+	unsigned long cJfFlag;                        //是否进行数据融合标志，0：未融合，1：已融合
 	unsigned long lFlag;                 //累加标记
+//1016补充
+	unsigned long lFusFlag;                //融合查找标记
 
 }TRACKSTATUS_MARK;
 
@@ -140,6 +154,84 @@ typedef vector<SHIP_POSITION> VCT_SHIP_POSITION; //存储各舰的GPS信息
 typedef vector<ESMSTATUS_MARK> VCT_ESM_MSG;	//存储雷达侦察信息
 typedef vector<COMSTATUS_MARK> VCT_COMM_MSG; //存储通信侦查信息
 typedef vector<TRACKSTATUS_MARK> VCT_TRACE_MSG; //存储雷达探测信息
+//////// 2014 1016 wanghaiying ///////////////////////////////////////////////////
+typedef struct __Cooperative_FUSIDENTIINFOR   //协同后，融合信息与识别结果结构体
+{
+    long int nStampTime;                     //发送请求信息时的当前时间 ，上述的当前时间可用返回信息的当前时间来代替	
+	unsigned long lAutonum;
+    char sPlatType[32];             //综合平台类型  F117  (F117； F118； F119； F120； F121)
+	double dConfidence;                      //综合可信度
+	
+	TRACKSTATUS_MARK stFusTrack;            //存储融合后的雷达探测信息（航迹信息）
+	VCT_ESM_MSG vctFusEsm;                  //存储融合后的雷达侦察信息
+	VCT_COMM_MSG vctFusCom;                 //存储融合后的通信侦查信息   
+}Cooperative_FUSIDENTIINFOR;
+
+typedef vector<Cooperative_FUSIDENTIINFOR> VCT_Cooperative_FUSIDENTIINFOR;  //存储协同后融合信息和识别结果的结构体
+
+typedef struct Link_Table_Event
+{
+	int nSerialnum;                       //序号
+    int nTrackMBHao;                      //航迹对应的模板号
+	int nTrackHao;                        //航迹批号
+	
+	int nTrackCumulativeN;                //航迹累积值
+	int nTrackBasicSituation;             //航迹推理的基本事件
+	
+	int nESMCount;                        //捆邦ESM数
+	VCT_INT vctESMMBHao;                      //ESM对应的ESM模板号
+	VCT_INT vctESMHao;                        //ESM批号
+	
+	VCT_INT vctESMCumulativeN;                //ESM累积值
+	VCT_INT vctESMBasicSituation;             //ESM基本事件
+	
+	int nCOMN;                            //捆邦数
+	VCT_INT vctCOMMBHao;                      //COM对应的COM模板号
+	VCT_INT vctCOMHao;                        //COM批号
+	
+	VCT_INT nCOMBasicSituation;               //COM基本事件
+	
+    double dThreatDegree;                 //平台综合威胁度
+	char cTrackType;                      //平台综合识别类型
+	
+}EVENTLINKS;  //放特征层基本事件链表节点数据;
+
+//基本态势：1：匀速直线运动、2：加速直线运行、3：俯冲、4、上升、5、拐弯、6：远离本舰、7：逼近本舰、
+	//          8：：位置固定、9：新目标、10：目标消失、11：不明
+
+//基本事件黑板结构
+typedef struct BasicEVentBIndexes 
+{
+	int nSeriaN;                        //序号
+	double dTimeStamp;                  //时戳
+	int nTrackHao;                      //航迹批号	
+	
+	//对应的航迹参数(空间聚类要用)
+	double dRd;                  //径距
+	double dAz;
+	double dEz;
+	double dHigh;               //高度
+	double dSpeed;              //速度
+	double dAddSpeed;           //加速度
+	double dAzimuth;            //航向
+	
+	
+	int nTrackEvent;           //航迹基本事件：
+	VCT_INT vctEventValue;        //航迹参数变化特征
+	
+    double dThreatDegree;          //平台综合威胁度
+    char cTrackType;               //航迹综合识别的目标类型
+	
+	//捆邦的异常ESM情况
+	int nESMCount;                 //ESM数
+	VCT_INT vctESMHao;                 //正常ESM批号与非正常的ESM,//捆邦的ESM异常不超过10个
+	VCT_INT vctESMEvent;               //ESM基本事件
+	//捆邦的异常COM情况
+	int nCOMCount;                 //COM数
+	VCT_INT vctCOMHao;                 //正常COM批号与非正常的COM
+	
+	VCT_INT vctCOMEvent;               //COM基本事件
+}EVENTBLACK;  //放黑板节点数据;
 
 //所有消息的头
 typedef struct _X_ALL_RECV_MSG
